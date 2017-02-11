9857f3de96c197b0155d259b215fcb11e8c5c0c8
JP Trawinski <jtrawins@oswego.edu>
2017-02-11 18:05:40 -0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -5,10 +5,8 @@ class Agent:
         self.util = util
 
     def setAdj(adj):
-        '''
-        sellTo is a list with True values for which agents
+        #sellTo is a list with True values for which agents
         it can sell to
-        '''
         self.sellTo = adj[self.index,] == 1.toList()
 
         #same as above
